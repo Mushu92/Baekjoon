5ee3e8b0828239660b74a84f0131a915ad2bc3bb
Taboo <mind9873@naver.com>
2018-01-30 21:21:44 +0900

    기타 폴더 추가, 10진수->2진수 코드
    10진수 -> 2진수  벡터에 결과 저장하지 말고 바로 출력하는 방법은 없을까

---
+++ 기타/10to2.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+	vector<int> result;
+	int num;
+	cin >> num;
+	while (num > 0) {
+		result.push_back(num % 2);
+		num /= 2;
+	}
+	for (int i = result.size()-1; i >= 0; --i) {
+		cout << result[i];
+	}
+	cout << endl;
+	system("pause");
+	return 0;
+}

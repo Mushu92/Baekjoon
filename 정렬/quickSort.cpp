5a3ca8b52436e7e6f42ca26467b226d355a9368f
Taboo <mind9873@naver.com>
2018-02-13 22:35:23 +0900

    백준 2751 풀이(quick sort)
    quick소트 정리, cin과 cout은 scanf와 printf에 비해서 동작이 느린 것 확인, cout에서 endl을 쓰는 경우에도 느려진다.(Time out 조심)

---
+++ 정렬/quickSort.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+
+using namespace std;
+
+void quickSort(int* inputarr, int low, int high);
+int partition(int* inputarr, int low, int high);
+
+int main() {
+	int num;
+	cin >> num;
+	//Init
+	int* inputarr = new int[num];
+	for (int i = 0; i < num; i++) {
+		int inum;
+		cin >> inum;
+		inputarr[i] = inum;
+	}
+	//Äü ¼ÒÆÃ
+	quickSort(inputarr, 0, num - 1);
+	//Ãâ·Â
+	for (int i = 0; i < num; i++) {
+		cout << inputarr[i] << "\n";
+	}
+	delete[] inputarr;
+	system("pause");
+	return 0;
+}
+
+void quickSort(int* inputarr, int low, int high) {
+
+	if (high > low) {
+		int pivotpoint = partition(inputarr, low, high);
+		quickSort(inputarr, low, pivotpoint - 1);
+		quickSort(inputarr, pivotpoint + 1, high);
+	}
+}
+
+int partition(int* inputarr, int low, int high) {
+	int j = low;
+	int pivotpoint;
+	int pivotitem = inputarr[low];
+	for (int i = low + 1; i <= high; i++) {
+		if (inputarr[i] < pivotitem) {
+			j++;
+			int temp = inputarr[j];
+			inputarr[j] = inputarr[i];
+			inputarr[i] = temp;
+		}
+	}
+	pivotpoint = j;
+	inputarr[low] = inputarr[j];
+	inputarr[j] = pivotitem;
+	return pivotpoint;
+}

93bee01fbf01172c09c8a691cdf95ed28941dccc
Taboo <mind9873@naver.com>
2018-02-27 00:57:47 +0900

    백준 9012해결
    런타임 문제 해결 그러나 코드 정리 필요

--- Stack/VPS.cpp
+++ Stack/VPS.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<string>
 #include<stack>
-#include<string.h>
 
 using namespace std;
 
@@ -9,43 +8,31 @@ using namespace std;
 int main() {
 	int num;
 	cin >> num;
-	string* res = new string[num];
-	int index = 0;
 	for (int i = 0; i < num; i++) {
 		//string 입력
-		char input[MAX_SIZE];
-		cin >> input;
-		int size = strlen(input);
+		string inputstr;
+		cin >> inputstr;
+		int size = inputstr.length();
+		bool flag=1;
 		//stack
 		stack<char> cstc;
 		for (int j = 0; j < size; j++) {
-			if (input[j] == '(') cstc.push(input[j]);
+			if (inputstr[j] == '(') cstc.push(inputstr[j]);
 
-			else if (input[j] == ')') {
-				int count = 0;
-				if (!cstc.empty()) {
-					while (cstc.top() != '(') {
-						count++;
-						cstc.pop();
-					}
-					cstc.pop();
-					while (count > 0) {
-						cstc.push(')');
-						count--;
-					}
+			else {//')'경우
+				if (!cstc.empty()) cstc.pop();
+				else {
+					flag = 0;//검증 끝!
+					break;
 				}
-				else cstc.push(input[j]);
 			}
 		}
-		if (cstc.empty()) res[index++] = "YES";
-		//'('가 남아있는 경우
-		else res[index++] = "NO";
+		//반복문 이후 스택이 비어있다면 "YES"
+		if(flag) flag = cstc.empty();
+		//출력
+		if (flag) cout << "YES\n";
+		else cout << "NO\n";
 	}
-	//출력
-	for (int i = 0; i < index; i++) {
-		cout << res[i] << endl;
-	}
-	delete[] res;
 	system("pause");
 	return 0;
 }

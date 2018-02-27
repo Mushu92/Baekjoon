9db5e5dfba76f2acb630b05ba600b356bee09491
Taboo <mind9873@naver.com>
2018-02-27 22:06:30 +0900

    refactoring
    리펙토링~

--- Stack/VPS.cpp
+++ Stack/VPS.cpp
@@ -3,8 +3,7 @@
 #include<stack>
 
 using namespace std;
-
-#define MAX_SIZE 100
+bool CheckVPS(string str);
 int main() {
 	int num;
 	cin >> num;
@@ -12,27 +11,28 @@ int main() {
 		//string 입력
 		string inputstr;
 		cin >> inputstr;
-		int size = inputstr.length();
-		bool flag=1;
-		//stack
-		stack<char> cstc;
-		for (int j = 0; j < size; j++) {
-			if (inputstr[j] == '(') cstc.push(inputstr[j]);
-
-			else {//')'경우
-				if (!cstc.empty()) cstc.pop();
-				else {
-					flag = 0;//검증 끝!
-					break;
-				}
-			}
-		}
-		//반복문 이후 스택이 비어있다면 "YES"
-		if(flag) flag = cstc.empty();
-		//출력
-		if (flag) cout << "YES\n";
+		if (CheckVPS(inputstr)) cout << "YES\n";
 		else cout << "NO\n";
 	}
 	system("pause");
 	return 0;
+}
+
+bool CheckVPS(string str) {
+	int size = str.length();
+	stack<char> cstc;
+	for (int j = 0; j < size; j++) {
+		//'('는 무조건 push
+		if (str[j] == '(') cstc.push(str[j]);
+
+		else {//')'경우 
+			//스택에 '('가 있다면 하나 제거
+			if (!cstc.empty()) cstc.pop();
+			else {//없는 경우라면 "NO"로 검증 완료
+				return 0;
+			}
+		}
+	}
+	//반복문 이후 스택이 비어있다면 "YES"
+	return cstc.empty();
 }
